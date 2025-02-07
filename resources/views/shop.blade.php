5161d887fdb1f99777209c98213a636ba7a2edcb
dionpool <elirionn@gmail.com>
2025-02-07 13:10:25 +0100

    Update shop page.

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app title="Shop">
     <section class="coming-soon">
-        <h2>Komt binnenkort.</h2>
+        <h2>Deze pagina wordt binnenkort vervangen.</h2>
     </section>
 </x-layouts.app>
